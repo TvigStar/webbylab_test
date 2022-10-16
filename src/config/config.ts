02f928fa78dbed044caa99e721a312c0f3e1691c
astr073 <astro73official@gmail.com>
2022-10-16 22:50:32 +0300

    init project

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,7 @@
 export const config = {
-  PORT: process.env.PORT || 5001,
-  HOST: process.env.PORT || 'http://localhost',
-  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5000',
+  PORT: process.env.APP_PORT || 8050,
 
-  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
+  ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:8000',
 
   JWT_SECRET: process.env.JWT_SECRET || 'uf7e^WaiUGFSA7fd8&^dadh',
   ACCESS_TOKEN_LIFETIME: process.env.ACCESS_TOKEN_LIFETIME || '20m',
